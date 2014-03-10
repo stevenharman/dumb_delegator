3b8886f5aee692a66aeef4022a11ee097c20894f
Steven Harman <steven@harmanly.com>
2014-03-10 17:56:54 -0400

    Use named subject for clarity.

--- spec/dumb_delegator_spec.rb
+++ spec/dumb_delegator_spec.rb
@@ -1,25 +1,25 @@
 require "spec_helper"
 
 describe DumbDelegator do
+  subject(:dummy) { described_class.new(target) }
   let(:target) { double }
-  subject { described_class.new(target) }
 
   it "delegates to the target object" do
     expect(target).to receive(:foo)
-    subject.foo
+    dummy.foo
   end
 
   it "delegates to the target object with arguments" do
     expect(target).to receive(:foo).with(:bar)
 
-    subject.foo(:bar)
+    dummy.foo(:bar)
   end
 
   it "delegates to the target object with a block" do
     bar_block = proc { "bar" }
     expect(target).to receive(:foo) { |&block| expect(block).to eq(bar_block) }
 
-    subject.foo(&bar_block)
+    dummy.foo(&bar_block)
   end
 
   it "does not delegate if the target does not respond_to? the message" do
@@ -27,67 +27,67 @@ describe DumbDelegator do
     allow(target).to receive(:respond_to?).with(:foo).and_return(false)
 
     expect {
-      subject.foo
+      dummy.foo
     }.to raise_error(NoMethodError)
   end
 
   it "responds to methods defined by child classes that add behavior" do
     expect(target).to receive(:foo).never
-    def subject.foo
+    def dummy.foo
       "bar"
     end
 
-    subject.foo
+    dummy.foo
   end
 
   it "delegates methods defined on Object" do
     expect(target).to receive(:class)
-    subject.class
+    dummy.class
   end
 
   it "delegates is_a?" do
     expect(target).to receive(:is_a?)
-    subject.is_a?
+    dummy.is_a?
   end
 
   it "delegates methods defined on Kernel" do
     expect(target).to receive(:print)
-    subject.print
+    dummy.print
   end
 
   it "delegates !" do
     expect(target).to receive(:!)
-    !subject
+    !dummy
   end
 
   it "delegates !=" do
     expect(target).to receive(:!=)
-    subject != 1
+    dummy != 1
   end
 
   it "delegates ==" do
     expect(target).to receive(:==)
-    subject == 1
+    dummy == 1
   end
 
   it "delegates ==" do
     expect(target).to receive(:==)
-    subject == 1
+    dummy == 1
   end
 
   it "delegates instance_eval" do
     expect(target).to receive(:instance_eval)
-    subject.instance_eval { true }
+    dummy.instance_eval { true }
   end
 
   it "delegates instance_exec" do
     expect(target).to receive(:instance_exec)
-    subject.instance_exec { true }
+    dummy.instance_exec { true }
   end
 
   describe "#dup" do
     it "returns a shallow of itself, the delegator (not the underlying object)", :objectspace => true do
-      dupped = subject.dup
+      dupped = dummy.dup
 
       expect(ObjectSpace.each_object(DumbDelegator).map(&:__id__)).to include dupped.__id__
     end
@@ -95,7 +95,7 @@ describe DumbDelegator do
 
   describe "#clone" do
     it "returns a shallow of itself, the delegator (not the underlying object)", :objectspace => true do
-      cloned = subject.clone
+      cloned = dummy.clone
 
       expect(ObjectSpace.each_object(DumbDelegator).map(&:__id__)).to include cloned.__id__
     end
@@ -105,7 +105,7 @@ describe DumbDelegator do
     let(:target) { Object.new }
 
     it "marshals and unmarshals itself, the delegator (not the underlying object)", :objectspace => true do
-      marshaled = Marshal.dump(subject)
+      marshaled = Marshal.dump(dummy)
       unmarshaled = Marshal.load(marshaled)
 
       expect(ObjectSpace.each_object(DumbDelegator).map(&:__id__)).to include unmarshaled.__id__
@@ -115,22 +115,22 @@ describe DumbDelegator do
   describe "#respond_to?" do
     [:equal?, :__id__, :__send__, :dup, :clone, :__getobj__, :__setobj__, :marshal_dump, :marshal_load, :respond_to?].each do |method|
       it "responds to #{method}" do
-        expect(subject.respond_to?(method)).to be_true
+        expect(dummy.respond_to?(method)).to be_true
       end
     end
 
     context "subclasses of DumbDelegator" do
-      subject { Class.new(DumbDelegator) { def foobar; end }.new([]) }
+      subject(:dummy) { Class.new(DumbDelegator) { def foobar; end }.new([]) }
 
       it "respond to methods defined on the subclass" do
-        expect(subject.respond_to?(:foobar)).to be_true
+        expect(dummy.respond_to?(:foobar)).to be_true
       end
     end
   end
 
   describe "#__getobj__" do
     it "returns the target object" do
-      expect(subject.__getobj__).to equal target
+      expect(dummy.__getobj__).to equal target
     end
   end
 
@@ -141,8 +141,8 @@ describe DumbDelegator do
       new_target = double
       expect(new_target).to receive(:foo)
 
-      subject.__setobj__(new_target)
-      subject.foo
+      dummy.__setobj__(new_target)
+      dummy.foo
     end
   end
 end
